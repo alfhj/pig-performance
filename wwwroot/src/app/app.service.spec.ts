16403efcb3949e9788b232c227bbd2842f6977cb
Andreas Haugli <haugli.andreas@gmail.com>
2023-03-04 00:47:28 +0100

    Fixed full width of header navbar

--- wwwroot/src/app/app.service.spec.ts
+++ wwwroot/src/app/app.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AppService } from './app.service';
+import { PigService } from './app.service';
 
 describe('AppService', () => {
-  let service: AppService;
+  let service: PigService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AppService);
+    service = TestBed.inject(PigService);
   });
 
   it('should be created', () => {
