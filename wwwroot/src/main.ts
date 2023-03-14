970ba8c2c673247bac51a7887f7af66929df9469
Alf Helge Jakobsen <alfern159@gmail.com>
2023-03-15 00:27:06 +0100

    Add Firestore support

--- wwwroot/src/main.ts
+++ wwwroot/src/main.ts
@@ -1,7 +1,34 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { initializeApp } from 'firebase/app';
+import {
+  collection,
+  Firestore,
+  getDocs,
+  getFirestore,
+} from 'firebase/firestore/lite';
 import { AppModule } from './app/app.module';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyANUTe_lBSvWBwJF0i06GCSfbBbqiT9T7E',
+  authDomain: 'pig-performance.firebaseapp.com',
+  projectId: 'pig-performance',
+  storageBucket: 'pig-performance.appspot.com',
+  messagingSenderId: '276860636304',
+  appId: '1:276860636304:web:52b7c07331b0ad22709c15',
+  measurementId: 'G-LCDXPP0BJZ',
+};
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const app = initializeApp(firebaseConfig);
+const db_instance = getFirestore(app);
+
+async function getFarms(db: Firestore) {
+  const farmsCol = collection(db, 'farms');
+  const farmSnapshot = await getDocs(farmsCol);
+  const farmList = farmSnapshot.docs.map((doc) => doc.data());
+  return farmList;
+}
+console.log(getFarms(db_instance));
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
