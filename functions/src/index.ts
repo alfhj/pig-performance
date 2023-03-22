20c56aa04b607b86d714a78e861d5ec2a244870b
Alf Helge Jakobsen <alfern159@gmail.com>
2023-03-22 23:21:59 +0100

    Write to database in function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,34 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
- // Start writing functions
- // https://firebase.google.com/docs/functions/typescript
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+/*
+type SpanPayload = {
+  messages: [
+    {
 
- export const payload = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
+    }
+  ]
+}
+*/
+
+admin.initializeApp();
+
+export const payload = functions.https.onRequest(async (request, response) => {
+  let message;
+  try {
+    message = request.body.messages[0];
+  } catch {
+    functions.logger.info("Invalid payload!", { structuredData: true });
+    response.status(400).send("Invalid payload");
+    return;
+  }
+  functions.logger.info("Hello logs!", { structuredData: true });
+  functions.logger.info(message, { structuredData: true });
+  await admin
+    .database()
+    .ref("payloads/" + message.messageId)
+    .set(message);
+  response.send("Thank you");
 });
