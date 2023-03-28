918af2ab5ef2a49b07fa120a6a792ba91bab3479
Andreas Haugli <haugli.andreas@gmail.com>
2023-03-29 00:35:40 +0200

    Displaing fetched data, need logic

--- wwwroot/src/app/home/home.component.ts
+++ wwwroot/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Database, get, onValue, ref } from '@angular/fire/database';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 // import {AddPostService} from '../add-post.service';
 // import {Observable} from 'rxjs';
 // import {PostPayload} from '../add-post/post-payload';
@@ -12,17 +14,32 @@ import { Router } from '@angular/router';
 
 export class HomeComponent {
 
-  // posts: Observable<Array<PostPayload>>;
-  // constructor(private postService: AddPostService) { }
+  public owner: string = "";
+  public location: string = "";
+  public jsonData: any;
+  public pigData: any[] = [];
 
-  // ngOnInit() {
-  //   this.posts = this.postService.getAllPosts();
-  // }
+  constructor(private router: Router, private database: Database) {
+    const dbRef = ref(this.database, 'farms');
 
-  // Fjern denne under
-
-  constructor(private router: Router) {
-    console.log('hey')
+    get(dbRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+        this.owner = snapshot.child('9cfc573e-d5fc-40e8-a9f1-7613e64f79a1/owner').val();
+        this.location = snapshot.child('9cfc573e-d5fc-40e8-a9f1-7613e64f79a1/location').val();
+        this.jsonData = (snapshot.child('9cfc573e-d5fc-40e8-a9f1-7613e64f79a1/pigs').val());
+        console.log(this.owner);
+        console.log(this.location);
+        for (let i in this.jsonData) {
+          this.pigData.push(this.jsonData[i])
+        }
+        console.log(this.pigData);
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
   };
 
   // Denne kan ta inn ID som parameter og navigere videre.
