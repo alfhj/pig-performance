2a5dee632440dd4e5771830afdbecb0b1be7b78c
torjeikenes <torje.n.eikenes@gmail.com>
2023-04-11 15:22:18 +0200

    combined serial receive and udp sending

--- arduino/SerialTest/src/main.cpp
+++ arduino/SerialTest/src/main.cpp
@@ -1,4 +1,35 @@
 #include <Arduino.h>
+#include <MKRNB.h>
+
+NB nbAccess(true);
+NBUDP udp;
+GPRS gprs;
+char apn[] = "mda.lab5e";               // replace with your APN name
+char server_address[] = "172.16.15.14"; // replace with your server's IP address
+int server_port = 1234;                 // replace with your server's port number
+char pin[] = "1112";
+
+void sendToSpan(String data)
+{
+  udp.begin(1232);
+  if (udp.beginPacket(server_address, server_port))
+  {
+
+    char *buf;
+    unsigned int bufsize;
+    data.toCharArray(buf, bufsize);
+
+    Serial.println("Sending UDP packet");
+    udp.write(buf, bufsize);
+    udp.endPacket();
+    Serial.println("Packet sent");
+  }
+  else
+  {
+    Serial.println("Unable to send UDP packet");
+  }
+  udp.stop();
+}
 
 void setup()
 {
@@ -8,6 +39,30 @@ void setup()
   delay(1000);
 
   Serial.println("Serial initiated");
+
+  // connection state
+  boolean connected = false;
+  // Serial.println("AT+CGDCONT=1,\"IP\",\"mda.lab5e\"");
+
+  // After starting the modem with NB.begin()
+  // attach to the GPRS network with the APN, login and password
+  Serial.println("Tryna connect");
+  while (!connected)
+  {
+    if ((nbAccess.begin(pin, apn) == NB_READY) && (gprs.attachGPRS() == GPRS_READY))
+    {
+      Serial.println("Attached to gprs");
+      connected = true;
+    }
+    else
+    {
+      Serial.println("Not connected");
+      delay(1000);
+    }
+  }
+  delay(2000);
+
+  Serial.println("Connected to the GPRS network");
 }
 
 void loop()
@@ -17,6 +72,6 @@ void loop()
   {
     String rx_string = Serial1.readString();
     Serial.println(rx_string);
-    // TODO: send rx_string to span
+    sendToSpan(rx_string);
   }
 }
